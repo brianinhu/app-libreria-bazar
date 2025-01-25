f607e420226c7d4ecf9cfb652ea2e3ca01647432
Brian <124319196+brianinhu@users.noreply.github.com>
2025-01-25 17:27:26 -0500

    delete JS uppercase dir

--- src/main/webapp/JS/admin/layout.js
+++
@@ -1,9 +0,0 @@
-var aside = document.getElementById("aside");
-var menu = document.getElementById("menu");
-
-var divpanel = document.getElementById("div-panel");
-
-menu.addEventListener("click", ()=>{
-    aside.classList.toggle("active");
-    divpanel.classList.toggle("decompressed");
-});
