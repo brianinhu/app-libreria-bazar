a86de0cd0806721f3a1b93e184c7c8080c85cdea
Brian Inca <brianincah10@gmail.com>
2025-02-21 20:32:57 -0500

    feat: add controller for index.jsp and categories

---
+++ src/main/java/libreria/controlador/HomeController.java
@@ -0,0 +1,25 @@
+package libreria.controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import libreria.modelo.bean.Categoria;
+import libreria.modelo.dao.CategoriaDAO;
+
+@WebServlet("")
+public class HomeController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        ArrayList<Categoria> categorias = new CategoriaDAO().toList();
+        request.setAttribute("categorias", categorias);
+
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+}
