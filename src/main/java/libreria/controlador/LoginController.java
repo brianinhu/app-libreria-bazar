cb0993c7611a3986a4f3ac644e6f4f153269bfa2
Brian <124319196+brianinhu@users.noreply.github.com>
2025-03-02 15:31:40 -0500

    style: nav and pages styles (#14)
    * feat: add categories for controllers
    
    * feat: add styles
    
    * refactor: autocomplete off for signup

--- src/main/java/libreria/controlador/LoginController.java
+++ src/main/java/libreria/controlador/LoginController.java
@@ -8,13 +8,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import libreria.modelo.bean.Carrito;
+import libreria.modelo.bean.Categoria;
 import libreria.modelo.bean.Cliente;
+import libreria.modelo.dao.CategoriaDAO;
 import libreria.modelo.dao.ClienteDAO;
 
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ArrayList<Categoria> categorias = new CategoriaDAO().toList();
+        request.setAttribute("categorias", categorias);
         request.getRequestDispatcher("login.jsp").forward(request, response);
     }
 
@@ -36,4 +41,3 @@ public class LoginController extends HttpServlet {
         }
     }
 }
-
--- src/main/java/libreria/controlador/SignupController.java
+++ src/main/java/libreria/controlador/SignupController.java
@@ -1,15 +1,15 @@
-
 package libreria.controlador;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import libreria.modelo.bean.Categoria;
 import libreria.modelo.bean.Cliente;
+import libreria.modelo.dao.CategoriaDAO;
 import libreria.modelo.dao.ClienteDAO;
 
 @WebServlet("/signup")
@@ -18,7 +18,9 @@ public class SignupController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("/signup.jsp").forward(request, response);
+        ArrayList<Categoria> categorias = new CategoriaDAO().toList();
+        request.setAttribute("categorias", categorias);
+        request.getRequestDispatcher("signup.jsp").forward(request, response);
     }
 
     @Override
